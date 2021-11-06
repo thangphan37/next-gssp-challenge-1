76940f6bf9185fff41a2c96a0c959782a3423fab
thangphan37 <pcthangk59@gmail.com>
2021-11-06 15:00:15 +0900

    add challenge

---
+++ pages/albums.tsx
@@ -0,0 +1,13 @@
+/**
+ * This is a free public API: https://jsonplaceholder.typicode.com/albumns
+ * Requirements:
+ * 	- Pre-render data of albums at the request time.
+ * Hint:
+ * 	- Using `getServerSideProps`.
+ */
+
+function Albums() {
+  return <div>{/* add your code here */}</div>
+}
+
+export default Albums
